6e510f1daebedce8b96124b2a21122de02491fa9
Marcus <vini.mark2002@gmail.com>
2022-05-27 18:52:08 -0300

    Exercicio de Módulos CommonJS

---
+++ Moderno (ES6+)/Exercícios/1-Modulos_CommonJS/src/Planet.js
@@ -0,0 +1,6 @@
+module.exports = class Planet {
+    constructor(name, starDistanceInAU) {
+        this.name = name,
+        this.starDistanceInAU = starDistanceInAU
+    }
+}
---
+++ Moderno (ES6+)/Exercícios/1-Modulos_CommonJS/src/index.js
@@ -0,0 +1,19 @@
+const Planet = require('./Planet')
+const PlanetOperations = require('./PlanetOperations')
+
+let planets = [
+    new Planet("Mercúrio", 0.39),
+    new Planet("Vênus", 0.72),
+    new Planet("Terra", 1),
+    new Planet("Marte", 1.52),
+    new Planet("Júpiter", 5.2),
+    new Planet("Saturno", 9.53),
+    new Planet("Urano", 19.1),
+    new Planet("Netuno", 30)
+]
+
+planets.forEach(planet => {
+    AUToKm = PlanetOperations.convertAUtoKm(planet.starDistanceInAU).toFixed(2)
+
+    console.log(`${planet.name} - Distância do Sol: ${planet.starDistanceInAU} AU (ou ${AUToKm} Km)`)
+});
