599eeb04bfbbe1f04f2204efb3af42d04e890003
Marcus <vini.mark2002@gmail.com>
2022-05-28 00:13:54 -0300

    Aula Promises

---
+++ Moderno (ES6+)/Aulas/16-Promises/src/index.js
@@ -0,0 +1,25 @@
+let komodoShip = {
+    name: "Komodo",
+    velocity: 80,
+    acceleration: 0,
+}
+
+const velocityAfter2Seconds = (velocity, acceleration) => {
+
+    new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            if (acceleration > 0) {
+                velocity += acceleration * 2
+                console.log(`Nova velocidade: ${velocity}`)
+                resolve(velocity)
+            } else {
+                console.log("Aceleração inválida")
+                reject("Não possui aceleração")
+            }
+        }, 1000)
+    })
+}
+
+velocityAfter2Seconds(komodoShip.velocity, komodoShip.acceleration)
+
+console.log("Execução de Promises")
