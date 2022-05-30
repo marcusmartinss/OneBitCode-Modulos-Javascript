5650528c8d759f36cee09cc98884278b0ccc56bc
Marcus <vini.mark2002@gmail.com>
2022-05-30 18:29:05 -0300

    Correção do babel.config.js

---
+++ Moderno (ES6+)/Aulas/18-Executando_varias_promises/src/index.js
@@ -0,0 +1,27 @@
+const increaseVelocity = function(velocityToIncrease) {
+    return new Promise ( (resolve, reject) => {
+        if (velocityToIncrease <= 0) {
+            reject("Frenagem acionada!")
+        } else {
+            resolve(velocityToIncrease * 0.9)
+        }
+    })
+}
+
+const newBatteryComsumption = function(currentVelocity, velocityToIncrease) {
+    return new Promise ( (resolve, reject) => {
+        let newBatteryComsumption = (currentVelocity + velocityToIncrease) / 10000
+        if (newBatteryComsumption > 0) {
+            resolve(newBatteryComsumption)
+        } else {
+            reject("Consumo zerado!")
+        }
+    })
+}
+
+let velocity = 70
+
+let velocityIncreased = increaseVelocity(velocity)
+let batteryComsumption = newBatteryComsumption(80, velocity)
+
+Promise.all([velocityIncreased, batteryComsumption])
