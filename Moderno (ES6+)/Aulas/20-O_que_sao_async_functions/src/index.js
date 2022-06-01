3e934b79fd24ffc68c4e51ae0be25cc445f32041
Marcus <vini.mark2002@gmail.com>
2022-06-01 18:38:18 -0300

    Aula O que é Await

--- Moderno (ES6+)/Aulas/20-O_que_sao_async_functions/src/index.js
+++ Moderno (ES6+)/Aulas/20-O_que_sao_async_functions/src/index.js
@@ -20,15 +20,15 @@ async function fire(x, y, z) {
     return([x, y, z])
 }
 
-function moveAndFire(x, y, z) {
-    adjustPosition(x, y, z).then(coord => {
-        console.log(`Arma ajustada para as coordenadas (${coord[0]}), (${coord[1]}), (${coord[2]})`)
-        return fire(...coord)
-    }).then(coord => {
-        console.log(`Começando a atirar nas coordenadas (${coord[0]}), (${coord[1]}), (${coord[2]})`)
-    }).catch(error => {
+async function moveAndFire(x, y, z) {
+    try {
+        let newCoordinates = await adjustPosition(x, y, z)
+        console.log(`Arma ajustada para as coordenadas (${newCoordinates[0]}), (${newCoordinates[1]}), (${newCoordinates[2]})`)
+        let fireCoordinates = await fire(...newCoordinates)
+        console.log(`Começando a atirar nas coordenadas (${fireCoordinates[0]}), (${fireCoordinates[1]}), (${fireCoordinates[2]})`)
+    } catch (error) {
         console.log(error)
-    })
+    }
 }
 
-moveAndFire(20, 40, 95)
+moveAndFire(20, 40, 90)
